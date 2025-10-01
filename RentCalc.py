6dcc97682d9941a9e05f6b6c2585cd5dd47dbe54
AdilY04 <adil.y04@outlook.com>
2025-10-01 16:05:28 +0100

    rent calc created

---
+++ RentCalc.py
@@ -0,0 +1,20 @@
+## Input we need from the user
+# Total rent
+# Total food ordered for snacking
+# Electricity units spend
+# Charge per unit
+
+## Output
+# Total amount you've got to pay
+
+rent = int(input("Enter your hostel/flat rent = "))
+food = int(input("Enter the amount of food ordered = "))
+electricity_spend = int(input("Enter the total of electricity spend = "))
+charge_per_unit = int(input("Enter the charge per unit = "))
+persons = int(input("Enter the number of people living in room/flats = "))
+
+total_bill = electricity_spend * charge_per_unit
+
+output = (food + rent + total_bill) // persons
+
+print(f"Each person will pay {output}")
--- test.py
+++
@@ -1,6 +0,0 @@
-print("Hello World.")
-print ("Hello Test")
-
-print("Hello World 22")
-
-print("Hello World from chrome")
