c045769308af6a5df79a7e7b8192901a2bc77c50
AdilY04 <adil.y04@outlook.com>
2025-10-02 11:33:49 +0100

    COMIT

--- ToDoList.py
+++ ToDoList.py
@@ -1,4 +1,7 @@
 '''
+
+PAUSED: RESUME AT HOME
+
 Beginner: Python To-Do List
 
 Create an empty list to store tasks
@@ -8,3 +11,24 @@ remove tasks and exit options.
 
 '''
 
+toDoList = []
+
+loop = "on"
+while loop == "on":
+    userinp = int(input("""
+Welcome to the Python to do list. Please enter:
+          1 - Add task
+          2 - View task
+          3 - Remove task
+          4 - Exit
+          """))
+    if userinp == 1:
+        break
+    if userinp == 2:
+        break
+    if userinp == 3:
+        break
+    if userinp == 4:
+        break
+    else:
+        print("Error.")
